45a58d8df7d00bc62a8235e94e197f5e9cf78d5e
Daniil <keipaglows@gmail.com>
2020-07-18 18:13:51 +0300

    RGBG-4 | Forgotten code

--- grid/grid_lines.gd
+++ grid/grid_lines.gd
@@ -6,8 +6,8 @@ enum { EMPTY = -1, GRID_LINE }
 
 func _ready():
 	if render_grid_lines:
-		var Grid = get_node("/root/Game/Grid")
-		var grid_rect = Grid.get_used_rect()
+		var main_grid = get_node("/root/Game/GridPrimary")
+		var grid_rect = main_grid.get_used_rect()
 		
 		for x in range(grid_rect.size[0]):
 			for y in range(grid_rect.size[1]):
